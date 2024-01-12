2b95320ed0003d3881acc23126fae014cbe4d405
Desislav Dimitrov <desko.desko192@gmail.com>
2024-01-12 22:27:15 +0200

    Calculate pins refactor

--- src/common/helpers/getLegalMoves.ts
+++ src/common/helpers/getLegalMoves.ts
@@ -1,7 +1,40 @@
-import type { PositionBoard, PieceColor, PieceBoard, PinTypes } from '../constants/constants';
+import type { PositionBoard, PieceColor, PieceBoard, PinTypes, Piece } from '../constants/constants';
 import type { BoardHistory } from '../../components/Board/Board';
 
 type CalculateColors = PieceColor | 'both';
+
+const setPins = (pins: PieceBoard[], piecesDir: PieceBoard[], pinTypeNext: keyof PinTypes, pinTypePrev: keyof PinTypes, pinners: Piece[]) => {
+	piecesDir.forEach((piece: PieceBoard, index: number) => {
+		const prevPiece = piecesDir[index - 1];
+		const nextPiece = piecesDir[index + 1];
+		
+		if(prevPiece && nextPiece) {
+			const checkPrev = (
+				prevPiece.piece === 'king' &&
+				prevPiece.color === piece.color &&
+				pinners.includes(nextPiece.piece) &&
+				nextPiece.color !== piece.color
+			);
+
+			const checkNext = (
+				nextPiece.piece === 'king' &&
+				nextPiece.color === piece.color &&
+				pinners.includes(prevPiece.piece) &&
+				prevPiece.color !== piece.color
+			);
+
+			if(checkPrev) {
+				piece.pins[pinTypeNext] = true;
+				if(!pins.includes(piece)) pins.push(piece);
+			}
+			
+			if(checkNext) {
+				piece.pins[pinTypePrev] = true;
+				if(!pins.includes(piece)) pins.push(piece);
+			}
+		}
+	});
+}
 
 const calculatePins = (latestPosition: PositionBoard, color: PieceColor) => {
     const pins: PieceBoard[] = [];
@@ -19,10 +52,6 @@ const calculatePins = (latestPosition: PositionBoard, color: PieceColor) => {
     const piecesDiagonalMain: PieceBoard[] = [];
     const piecesDiagonalOpp: PieceBoard[] = [];
 
-    // const setPins = (piece: PieceBoard, prevPiece: PieceBoard, nextPiece: PieceBoard, pinName: keyof PinTypes) => {
-
-    // }
-
     let countMain = 0;
     let countOpp = 0;
 
@@ -38,129 +67,24 @@ const calculatePins = (latestPosition: PositionBoard, color: PieceColor) => {
         countOpp++;
     }
 
-    piecesDiagonalMain.sort((a, b) => a.x - b.x)
-    piecesDiagonalOpp.sort((a, b) => b.x - a.x)
+    piecesDiagonalMain.sort((a, b) => a.x - b.x);
+    piecesDiagonalOpp.sort((a, b) => b.x - a.x);
 
-    piecesCol.forEach((piece: PieceBoard, index: number) => {
-        const prevPiece = piecesCol[index - 1];
-        const nextPiece = piecesCol[index + 1];
-        
-        if(prevPiece && nextPiece) {
-            if(
-                prevPiece.piece === 'king' &&
-                prevPiece.color === piece.color &&
-                (nextPiece.piece === 'queen' || nextPiece.piece === 'rook') &&
-                nextPiece.color !== piece.color
-            ) {
-                piece.pins.bottomVertical = true;
-                if(!pins.includes(piece)) pins.push(piece);
-            }
-            
-            if(
-                nextPiece.piece === 'king' &&
-                nextPiece.color === piece.color &&
-                (prevPiece.piece === 'queen' || prevPiece.piece === 'rook') &&
-                prevPiece.color !== piece.color
-                ) {
-                    piece.pins.topVertical = true;
-                    if(!pins.includes(piece)) pins.push(piece);
-            }
-        }
-    });
-
-    piecesRow.forEach((piece: PieceBoard, index: number) => {
-        const prevPiece = piecesRow[index - 1];
-        const nextPiece = piecesRow[index + 1];
-        
-        if(prevPiece && nextPiece) {
-            if(
-                prevPiece.piece === 'king' &&
-                prevPiece.color === piece.color &&
-                (nextPiece.piece === 'queen' || nextPiece.piece === 'rook') &&
-                nextPiece.color !== piece.color
-            ) {
-                piece.pins.rightHorizontal = true;
-                if(!pins.includes(piece)) pins.push(piece);
-            }
-            
-            if(
-                nextPiece.piece === 'king' &&
-                nextPiece.color === piece.color &&
-                (prevPiece.piece === 'queen' || prevPiece.piece === 'rook') &&
-                prevPiece.color !== piece.color
-            ) {
-                piece.pins.leftHorizontal = true;
-                if(!pins.includes(piece)) pins.push(piece);
-            }
-        }
-    });
-
-    piecesDiagonalMain.forEach((piece: PieceBoard, index: number) => {
-        const prevPiece = piecesDiagonalMain[index - 1];
-        const nextPiece = piecesDiagonalMain[index + 1];        
-        
-        if(prevPiece && nextPiece) {
-            if(
-                prevPiece.piece === 'king' &&
-                prevPiece.color === piece.color &&
-                (nextPiece.piece === 'queen' || nextPiece.piece === 'bishop') &&
-                nextPiece.color !== piece.color
-            ) {
-                piece.pins.RBDiagonal = true;
-                if(!pins.includes(piece)) pins.push(piece);
-            }
-            
-            if(
-                nextPiece.piece === 'king' &&
-                nextPiece.color === piece.color &&
-                (prevPiece.piece === 'queen' || prevPiece.piece === 'bishop') &&
-                prevPiece.color !== piece.color
-            ) {
-                piece.pins.LTDiagonal = true;
-                if(!pins.includes(piece)) pins.push(piece);
-            }
-        }
-    });
-
-    piecesDiagonalOpp.forEach((piece: PieceBoard, index: number) => {
-        const prevPiece = piecesDiagonalOpp[index - 1];
-        const nextPiece = piecesDiagonalOpp[index + 1];
-        
-        if(prevPiece && nextPiece) {
-            if(
-                prevPiece.piece === 'king' &&
-                prevPiece.color === piece.color &&
-                (nextPiece.piece === 'queen' || nextPiece.piece === 'bishop') &&
-                nextPiece.color !== piece.color
-            ) {
-                piece.pins.LBDiagonal = true;
-                if(!pins.includes(piece)) pins.push(piece);
-            }
-            
-            if(
-                nextPiece.piece === 'king' &&
-                nextPiece.color === piece.color &&
-                (prevPiece.piece === 'queen' || prevPiece.piece === 'bishop') &&
-                prevPiece.color !== piece.color
-            ) {
-                piece.pins.RTDiagonal = true;
-                if(!pins.includes(piece)) pins.push(piece);
-            }
-        }
-    });
-
-    console.log(pins);
-    
+	setPins(pins, piecesCol, 'bottomVertical', 'topVertical', ['queen', 'rook']);
+	setPins(pins, piecesRow, 'rightHorizontal', 'leftHorizontal', ['queen', 'rook']);
+	setPins(pins, piecesDiagonalMain, 'RBDiagonal', 'LTDiagonal', ['queen', 'bishop']);
+	setPins(pins, piecesDiagonalOpp, 'LBDiagonal', 'RTDiagonal', ['queen', 'bishop']);
+	
     return pins;
 }
 
 const calculatePawn = (positionHistory: BoardHistory, piece: PieceBoard) => {
-    // const {y, x} = piece;
-    const latestPosition: PositionBoard[] = [];
-    const previousPosition: PositionBoard[] = [];
+	// const {y, x} = piece;
+	const latestPosition: PositionBoard[] = [];
+	const previousPosition: PositionBoard[] = [];
 
-    if(positionHistory.length > 0) latestPosition.push(positionHistory[positionHistory.length - 1])
-    if(positionHistory.length > 1) previousPosition.push(positionHistory[positionHistory.length - 2])
+	if(positionHistory.length > 0) latestPosition.push(positionHistory[positionHistory.length - 1])
+	if(positionHistory.length > 1) previousPosition.push(positionHistory[positionHistory.length - 2])
 
 };
 
@@ -175,30 +99,33 @@ const calculateQueen = (positionHistory: BoardHistory, piece: PieceBoard) => {};
 const calculateKing = (positionHistory: BoardHistory, piece: PieceBoard) => {};
 
 const moveMap = {
-    pawn: calculatePawn,
-    bishop: calculateBishop,
-    knight: calculateKnight,
-    rook: calculateRook,
-    queen: calculateQueen,
-    king: calculateKing,
+	pawn: calculatePawn,
+	bishop: calculateBishop,
+	knight: calculateKnight,
+	rook: calculateRook,
+	queen: calculateQueen,
+	king: calculateKing,
 };
 
 const getLegalMoves = (positionHistory: BoardHistory, color: CalculateColors = 'both') => {
 	const piecesToCalculate = color !== 'both' ? positionHistory[positionHistory.length - 1].filter((piece: PieceBoard) => piece.color === color) : positionHistory[positionHistory.length - 1]
 
-    const latestPosition: PositionBoard[] = [];
-    const previousPosition: PositionBoard[] = [];
+	const latestPosition: PositionBoard[] = [];
+	const previousPosition: PositionBoard[] = [];
 
-    if(positionHistory.length > 0) latestPosition.push(positionHistory[positionHistory.length - 1])
-    if(positionHistory.length > 1) previousPosition.push(positionHistory[positionHistory.length - 2])
-    
-    piecesToCalculate.forEach((piece: PieceBoard) => {
-        moveMap[piece.piece](positionHistory, piece);
-    });
+	if(positionHistory.length > 0) latestPosition.push(positionHistory[positionHistory.length - 1])
+	if(positionHistory.length > 1) previousPosition.push(positionHistory[positionHistory.length - 2])
+	
+	piecesToCalculate.forEach((piece: PieceBoard) => {
+		moveMap[piece.piece](positionHistory, piece);
+	});
 
-    if(color !== 'both') {
-        calculatePins(latestPosition[0], color);
-    }
+	if(color !== 'both') {
+		calculatePins(latestPosition[0], color);
+	} else {
+		const blackPins = calculatePins(latestPosition[0], 'black');
+		const whitePins = calculatePins(latestPosition[0], 'white');
+	}
 };
 
 export default getLegalMoves;
